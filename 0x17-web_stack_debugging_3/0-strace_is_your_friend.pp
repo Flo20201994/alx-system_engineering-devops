d4adad253cefb993424eaf42aa3db7164dd59cef
Florence <florenceobirimainoo@gmail.com>
2024-09-25 17:14:02 -0700

    Add initial commit & 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,8 +2,4 @@
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
   path    => ['/bin', '/usr/bin', '/usr/sbin']
-# Fixes a wordpress site running on apache2
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
-}}
+}
